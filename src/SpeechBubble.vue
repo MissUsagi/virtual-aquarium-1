959a440f4c234cb912dfa04a2974b6dfea35475e
MissUsagi <syljanko@gmail.com>
2024-08-20 15:20:07 +0200

    feat: speech bubble

---
+++ src/SpeechBubble.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="speech-bubble down">
+    <p class="speech-bubble-text">
+      Feed me<span class="speech-bubble-text accent">!</span>
+    </p>
+  </div>
+</template>
+
+<style>
+.speech-bubble {
+  position: relative;
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 8px 10px;
+  border-radius: 15px;
+}
+
+.speech-bubble.down::after {
+  content: "";
+  position: absolute;
+  width: 0;
+  bottom: -6px;
+  right: 25%;
+  border: 6px solid transparent;
+  border-top-color: rgba(255, 255, 255, 0.8);
+  border-bottom: 0;
+}
+
+.speech-bubble-text {
+  font-weight: 600;
+}
+
+.speech-bubble-text.accent {
+  font-weight: 800;
+  color: red;
+}
+</style>
--- src/randomFishNames.js
+++ src/randomFishNames.js
@@ -1,3 +1,3 @@
-const randomFishNames = ["Bubbles", "Aquablade","Fishy", "Aquaflora", "Reefmaster", "Coralheart", "Marigold", "Silverscale", "Sunny", "Wavebouncer", "Lila", "Finn", "Lagoona", "Sirenity"]
+const randomFishNames = ["Bubbles", "Aquablade","Fishy", "Aquaflora", "Reefmaster", "Coralheart", "Marigold", "Silverscale", "Sunny", "Wavebouncer", "Laila", "Finn", "Lagoona", "Sirenity"]
 
 export default randomFishNames;
